482dfbbd2acd9c0b2d1b4df6ac055a4e959726e1
Eric <eric@quartiermaitre.com>
2015-12-29 00:58:19 +0100

    Ajout de la page de login et séparation du code javascript par page html

--- php/domotix.login.php
+++ php/domotix.login.php
@@ -1,12 +1,14 @@
 <?php
 	
 	include "domotix.cfg";
+
 		
 	$login =(isset($_GET['login'])) ? $_GET['login'] : $argv[1];
 	$pwd =(isset($_GET['pwd'])) ? $_GET['pwd'] : $argv[2];
 	
 	$db = new sqlite3( $GLOBALS['dbaseName']);
-	$qry = "SELECT count(*) FROM utilisateurs where login = '$login' and pwd = '$pwd'";	
+	$qry = "SELECT count(*) FROM utilisateurs WHERE login='$login' and pwd='$pwd'";	
+	
 	$rows = $db->query($qry);
 	$row = $rows->fetchArray();	
 	$numRows = $row[0];
@@ -14,10 +16,5 @@
 	
 	echo "$numRows";
 ?>
-
-		
-		
-
-
 
 
